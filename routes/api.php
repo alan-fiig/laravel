6a2495c0dc4dc32803a9483df9522d594d4d1fc3
Alan Figueroa Iglesias <alan.figueroa@hotmail.com>
2024-02-22 15:12:18 -0600

    User Login

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\ProductController;
 use App\Http\Controllers\CursoController;
+use App\Http\Controllers\UserController;
 use App\Models\User;
 
 
@@ -19,24 +20,32 @@ use App\Models\User;
 |
 */
 
-Route::apiResource('products', ProductController::class);
+// Route::apiResource('products', ProductController::class);
 
-Route::post('/products/signed/{id}', 'App\Http\Controllers\api\ProductController@generarUrlFirmada');
-
-Route::get('/products/show/{id}', [ProductController::class, 'show'])->name('products.show.signed')->middleware('signed', 'throttle:5,1');
-
-Route::middleware(['throttle:5,1'])->group(function () {
-    Route::get('/products', [ProductController::class, 'index']);
+Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/cursos', [CursoController::class, 'index']);
+
+    Route::apiResource('products', ProductController::class);
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/products/signed/{id}', 'App\Http\Controllers\api\ProductController@generarUrlFirmada');
+    Route::get('/products/show/{id}', [ProductController::class, 'show'])->name('products.show.signed')->middleware('signed', 'throttle:5,1');
+    Route::get('/products/description-length/{length}', [ProductController::class, 'getProductsByDescriptionLength']);
 });
+
+Route::post("login", [UserController::class, 'index']);
+
+
+// Route::middleware(['throttle:5,1'])->group(function () {
+//     Route::get('/products', [ProductController::class, 'index']);
+//     Route::get('/cursos', [CursoController::class, 'index']);
+// });
 
 Route::get('/users', function () {
     $users = User::all();
     return response()->json(['users' => $users]);
 })->name('users.index')->middleware('custom.restrict');
 
-Route::get('/products/description-length/{length}', [ProductController::class, 'getProductsByDescriptionLength']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
